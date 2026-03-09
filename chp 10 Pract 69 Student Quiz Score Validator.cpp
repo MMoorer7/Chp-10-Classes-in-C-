65d48a0f619f9efa5719b19de41574d3d2072146
MMoorer7 <mmoorer7@aum.edu>
2026-03-09 14:22:42 -0500

    Add files via upload

---
+++ chp 10 Pract 69 Student Quiz Score Validator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class QuizResult
+{
+private:
+    int score = 0;   
+
+public:
+    void set_score(int new_score)
+    {
+        if (new_score >= 0 && new_score <= 100)
+        {
+            score = new_score;
+        }
+        else
+        {
+            cout << "Invalid score!" << endl;
+        }
+    }
+
+    int get_score()
+    {
+        return score;
+    }
+};
+
+int main()
+{
+    QuizResult quiz;
+
+    int input_score;
+    cin >> input_score;
+
+    quiz.set_score(input_score);
+
+    cout << "Final score: " << quiz.get_score() << endl;
+
+    return 0;
+}
